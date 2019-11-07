249f8647b1f6dbee95073dbceb47679a7fef9938
Eric Energy <ericnrgnash@gmail.com>
2019-11-07 14:15:41 -0600

    finished product

--- Develop/main.js
+++ Develop/main.js
@@ -1,67 +1,86 @@
-var charNum = prompt("Enter the number of characters needed");
+var charNum;
 var ranNumbers = "0123456789".split('');
 var ranLetter = "abcdefghijklmnopqrstuvwxyz".split('');
 var ranLetterCap = "ZYXWVUTSRQPONMLKJIHGFEDCBA".split('');
 var ranSpecChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split('');
-var numbers = true; //confirm("Does it need to have numbers?\nPress ok for Yes or Canel for no. ");
-var lowCase = true; //confirm("Does it need to have Lower Case letters?\nPress ok for Yes or Canel for no. ");
-var uppCase = true; //confirm("Does it need to have Upper Case letters?\nPress ok for Yes or Canel for no. ");
-var specChar = true; //confirm("Does it need to have a special charcter?\nPress ok for Yes or Canel for no. ");
 var password = "";
 var availableCharacters = [];
-j = charNum;
 
 
 
-//if ( true) {
-    availableCharacters = availableCharacters.concat(ranNumbers); //random number generator
+var criteria = {
 
- // for (var i = 0; i < j; i++) {
+    copyTextToPaste: function() {
+
+    copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange (0,99999);
+    document.execCommand ("copy");
+    console.log(copyText)
+    },
+
+
+
+    buttonpressed: function() {
+    charNum = prompt("Enter the number of characters needed");
+    numbers = confirm("Does it need to have numbers?\nPress ok for Yes or Canel for no. ");
+    lowCase = confirm("Does it need to have Lower Case letters?\nPress ok for Yes or Canel for no. ");
+    uppCase = confirm("Does it need to have Upper Case letters?\nPress ok for Yes or Canel for no. ");
+    specChar = confirm("Does it need to have a special charcter?\nPress ok for Yes or Canel for no. ");
+    for (var i = 0; i < parseInt(charNum); i++) {
 
         
 
-        if (numbers) {
+        if ((password.length < charNum) && numbers) {
 
-            for (var i = 0; i < 1; i++) {
+            for (var j = 0; j < 1; j++) {
                 password = password + availableCharacters[Math.floor(Math.random() * availableCharacters.length)];
 
             }
         }
+        
+        if ((password.length < charNum) && lowCase)  {
 
-        if (lowCase) {
-
-            for (var i = 0; i < 1; i++) {
+            for (var j = 0; j < 1; j++) {
                 password = password + ranLetter[Math.floor(Math.random() * availableCharacters.length)];
 
             }
         }
+        
+        if ((password.length < charNum) && uppCase) {
 
-        if (uppCase) {
-
-            for (var i = 0; i < 1; i++) {
+            for (var j = 0; j < 1; j++) {
                 password = password + ranLetterCap[Math.floor(Math.random() * availableCharacters.length)];
 
             }
         }
 
-        if (specChar) {
+        if ((password.length < charNum) && specChar) {
 
-            for (var i = 0; i < 1; i++) {
+            for (var j = 0; j < 1; j++) {
                 password = password + ranSpecChar[Math.floor(Math.random() * availableCharacters.length)];
 
             }
         } document.getElementById('password').value = password;
-   // }
+    }
+    }
+
+}
+
+ 
     
-//}
+
+    availableCharacters = availableCharacters.concat(ranNumbers); //random number generator
+
+   
 
 
-    
 
 
-    console.log(password);
-    console.log(j);
+
+    console.log(password); 
     console.log(charNum);
+    console.log(password.length);
 
 
 
